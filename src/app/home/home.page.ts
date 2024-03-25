8389fd89d8a83e30f61932bef42609b3ab07212b
lfrz <anonimus47@hotmail.com>
2024-03-25 09:33:08 -0500

    Ionic flash light

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  active: boolean;
+
+  constructor(private flashlight: Flashlight) {
+    this.active = false;
+  }
+
+  flash() {
+    if (this.active) {
+      this.flashlight.switchOn();
+    } else {
+      this.flashlight.switchOff();
+    }
+  }
+}
